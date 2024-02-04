d40d2314cca5c41d7a543c1ae2841817489c7082
k-bar-n <bard.kosty@gmail.com>
2024-02-04 22:48:22 +0300

    some-changes---fixed-a-problem-in-the-code

--- main.py
+++ main.py
@@ -153,7 +153,7 @@ def zapusk_read_csv():
 
 
 # Функция для чтения CSV файла
-def read_csv(file_name, row_number=None):
+def read_csv(file_name, row_number):
     # Определение текущей директории скрипта
     script_dir = os.path.dirname(__file__)
     # Формирование пути к файлу CSV в папке static/data
@@ -165,20 +165,16 @@ def read_csv(file_name, row_number=None):
     with open(file_path, 'r', newline='', encoding='utf-8-sig') as file:
         # Использование csv.reader для разделения строк на элементы с разделителем ';'
         csv_reader = csv.reader(file, delimiter=';')
-        # Чтение заголовка и добавление его в список csv_data
-        header = next(csv_reader)
-        csv_data.append(header)
 
         # Если задан номер строки (row_number), читаем только эту строку
         if row_number is not None:
             for i, row in enumerate(csv_reader):
-                if i == row_number - 1:  # -1, так как индексы начинаются с 0
-                    csv_data.append(row)
+                if i == row_number:
+                    csv_data.extend(row)
                     break
         else:
-            # Итерация по строкам CSV и добавление их в список csv_data
-            for row in csv_reader:
-                csv_data.append(row)
+            # Если row_number не указан, возвращаем пустой список
+            return []
 
     # Возвращение полученных данных из CSV файла
     return csv_data
@@ -187,18 +183,18 @@ def read_csv(file_name, row_number=None):
 # Функция для запуска чтения CSV файла
 def zapusk_read_csv(row_number_to_extract):
     # Указание имени файла CSV, который будет прочитан
-    csv_file_name = 'Krepozh — копия (2).csv'
+    csv_file_name = 'Крепёж.csv'
 
     # Задание номера строки, которую вы хотите извлечь (замените на нужный номер) = row_number_to_extract
 
     # Вызов функции read_csv для чтения файла и сохранения данных в переменной data_csv_base
-    data_csv_row = read_csv(csv_file_name, row_number=row_number_to_extract)
+    data_csv_row = read_csv(csv_file_name, row_number_to_extract)
 
     # Вывод данных CSV в консоль (может быть изменено или использовано по вашему усмотрению)
     print(data_csv_row)
 
     # Вызов функции read_csv для чтения файла и возврат данных
-    return read_csv(csv_file_name, row_number=row_number_to_extract)
+    return data_csv_row
 
 
 '''
